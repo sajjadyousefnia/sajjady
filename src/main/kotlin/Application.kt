45144588de8204b54f5702c671d6e2638323419b
sajjadyousefnia <sdyussuf@gmail.com>
2019-09-04 17:40:10 +0430

    - next

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -17,21 +17,27 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 val port = System.getenv("PORT")?.toInt() ?: 23567
-fun main(args: Array<String>) {
-    embeddedServer(Netty, port) {
-        routing {
-            get("/") {
-                call.respond("I'm alive!")
+
+object MyClass {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        embeddedServer(Netty, port) {
+            routing {
+                get("/") {
+                    call.respond("I'm alive!")
+                }
+                get("/hello") {
+                    call.respond(HttpStatusCode.Accepted, "Hello")
+                }
+                get("random/{min}/{max}") {
+                    val min = call.parameters["min"]?.toIntOrNull() ?: 0
+                    val max = call.parameters["max"]?.toIntOrNull() ?: 10
+                    val randomString = "${(min until max).shuffled().last()}"
+                    call.respond(randomString)
+                }
             }
-            get("/hello") {
-                call.respond(HttpStatusCode.Accepted, "Hello")
-            }
-            get("random/{min}/{max}") {
-                val min = call.parameters["min"]?.toIntOrNull() ?: 0
-                val max = call.parameters["max"]?.toIntOrNull() ?: 10
-                val randomString = "${(min until max).shuffled().last()}"
-                call.respond(randomString)
-            }
-        }
-    }.start(wait = true)
+        }.start(wait = true)
+    }
+
 }
+
